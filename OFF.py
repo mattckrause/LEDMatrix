de2e08c5c3224204893bf0a3513c71ebd75d4921
Matt Krause <matthew.krause@outlook.com>
2022-08-21 23:03:06 -0600

    LED OFF

---
+++ OFF.py
@@ -0,0 +1,28 @@
+﻿# This is my signature purple crown as seen on the PiCast Show
+import time
+import board
+import neopixel
+ 
+ 
+# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
+pixel_pin = board.D18
+ 
+# The number of NeoPixels
+num_pixels = 256
+ 
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+ORDER = neopixel.GRB
+ 
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.3, auto_write=False, pixel_order=ORDER
+)
+
+BLACK = 0x000000
+GREEN = 0x025900
+
+for i in range(256):
+    pixels[i] = GREEN
+pixels.show()
+time.sleep(2)

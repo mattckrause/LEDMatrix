f11820ba74d6e474da0f7ec13044dbbd599d530c
Matt Krause <matthew.krause@outlook.com>
2022-08-22 22:12:47 -0600

    updates

--- KnightRider.py
+++ KnightRider.py
@@ -1,1 +1,24 @@
-﻿
+﻿# This is my signature purple crown as seen on the PiCast Show
+import time
+import board
+import neopixel 
+
+# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
+pixel_pin = board.D18
+num_pixels = 256
+
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+ORDER = neopixel.GRB
+
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.3, auto_write=False, pixel_order=ORDER
+)
+
+RED = 0xff0000
+
+for i in range(0,23):
+    pixels[i] = RED
+pixels.show()
+time.sleep(2)
